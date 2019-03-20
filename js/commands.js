b4d5cac599a6990b2a2a6966d81ee3e412630fea
Raghav Sharma <raghavsyt@gmail.com>
2019-03-20 19:03:15 +0530

    newline bug fixed

---
+++ js/commands.js
@@ -0,0 +1,1 @@
+const helpInput = '<ul><li><b>ls</b> - display alls files in current directory</li><br><li><b>cat FILENAME</b> - displays the file</li><br><li><b>cd DIRECTORY</b> - changes the current working directory</li><br></ul>';
--- js/script.js
+++ js/script.js
@@ -1,33 +1,50 @@
-window.addEventListener("load", function (event) {
-  first();
-});
+var count = 0;
+var history = [];
+newChild();
 
-function first() {
-  var first = document.getElementById("input");
-  first.contentEditable = true;
-  first.focus();
+function newChild() {
+  let div = document.createElement('div');
+  div.setAttribute("id", 'cmd' + count);
+  let first = document.createElement('span');
+  let second = document.createElement('span');
+  let third = document.createElement('span');
+
+  first.innerHTML = '<span class="root" id="root">root</span>';
+  second.innerHTML = '<span class="colon">:<span class="tild">~</span>$&nbsp;</span>';
+
+  third.setAttribute("class", "input");
+  third.setAttribute("contenteditable", true);
+  third.setAttribute("id", count);
+
+  document.getElementById("cmd").appendChild(div);
+  document.getElementById("cmd" + count).appendChild(first);
+  document.getElementById("cmd" + count).appendChild(second);
+  document.getElementById("cmd" + count).appendChild(third);
+
+  third.focus();
 }
-var input = document.getElementById("input");
-input.addEventListener("keyup", function (event) {
-  if (event.keyCode === 13) {
-    event.preventDefault();
-    var inputValue = input.innerText;
-    newAppend(inputValue);
-  }
-});
 const helpInput = '<ul><li><b>ls</b> - display alls files in current directory</li><br><li><b>cat FILENAME</b> - displays the file</li><br><li><b>cd DIRECTORY</b> - changes the current working directory</li><br></ul>';
 
-function newAppend(value) {
+var eListen = document.getElementById("cmd");
+eListen.addEventListener("keyup", function (event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    var inputText = document.getElementById(count).innerText;
+    history[count] = inputText;
+    console.log(inputText);
 
-  var insert = document.createElement("div");
-  var str = '<p><span class="root">root</span> <span class="tick">:<span class="tild">~</span>$ &nbsp;</span> <span contenteditable="true" id="input">lorem</span></p>';
-  console.log("Value entered is " + value);
-  if (value == input.innerText) {
-    insert.innerHTML = helpInput;
-  } else insert.innerHTML = str;
-  document.getElementById("line").appendChild(insert);
-  insert.focus();
-}
-
-// TODO event listener on input id, and when entered is press take input and parse it.
-// TODO if input is help then append a ul of having things we want to do.
+    document.getElementById(count).setAttribute("contenteditable", false);
+    if (inputText.trim() == "help") {
+      document.getElementById(count).insertAdjacentHTML("afterend", helpInput);
+    }
+    if (inputText.trim() == "history") {
+      console.log(history);
+      //document.getElementById(count).insertAdjacentHTML("afterend", history);
+    }
+    // if (inputText.trim() == "cd") {
+    //   document.getElementById("root").textContent = "home";
+    // }
+    ++count;
+    newChild();
+  }
+});
